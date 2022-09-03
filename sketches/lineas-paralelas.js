41c84c4f1cb1876f65968d21deac60c2c62df9dd
jalarconb <jalarconb@users.noreply.github.com>
2022-09-03 01:30:08 +0000

    deploy: dd37196f362812f33cc05aeaa9434a88bf08de51

--- sketches/lineas-paralelas.js
+++ sketches/lineas-paralelas.js
@@ -1,44 +1,64 @@
+let x = 15 // Desfase horizontal entre cada fila de cuadrados
+let y = 40; // Altura de las filas
+let fillVar = 0; // Relleno de los cuadrados
+let button;	// Botón para cambiar fillVar
+
 function setup() {
-    createCanvas(558, 360, WEBGL);
-  }
+	createCanvas(570, 400);
+	button = createButton('Mostrar/Ocultar Cuadrados');
+	button.position(0, 360);
+	button.size(width,40);
+  button.mousePressed(changeFillVar);
+}
   
 function draw() {
-    background(255);
+	background(255);
 
-    let x = 10;
-    let y = 40;
-
-    push();
-        noStroke();
-        fill(0);    
-        drawSquares(x,y);
-    pop();
-
-    push();
-        strokeWeight(3);
-        stroke(120);
-        for (let i = -3; i < 5; i++) {
-            line(-width/2,y*i-20,width/2,y*i-20);
-        }
-    pop();
+	drawSquares(x,y,fillVar);
+	drawLines(y);
 }
 
-function drawSquares(x,y){
-    for (let i = 0; i < 2; i++) {
-        for (let j = 0; j < 7; j++) {
-            square(-width/2+y/4+y*2*j,-height/2+y*4*i,y);
-        }
-        for (let j = 0; j < 7; j++) {
-            square(-width/2+y/4+y*2*j+x,-height/2+y+y*4*i,y);
-        }
-        for (let j = 0; j < 7; j++) {
-            square(-width/2+y/4+y*2*j+2*x,-height/2+2*y+y*4*i,y);
-        }
-        for (let j = 0; j < 7; j++) {
-            square(-width/2+y/4+y*2*j+x,-height/2+3*y+y*4*i,y);
-        }
-    }
-    for (let j = 0; j < 7; j++) {
-        square(-width/2+y/4+y*2*j,-height/2+y*8,y);
-    }
+// -----------------------------------------------------------------------
+
+function changeFillVar() { // Listener del botón para cambiar fillVar
+	if (fillVar === 0) {
+		fillVar = 255;
+	} else {
+		fillVar = 0;
+	}
+}
+
+function drawSquares(x,y,fillVar){ // Función para dibujar los cuadrados negros
+	push();
+		translate(y/4,0);
+		noStroke();
+		fill(fillVar);
+		for (let i = 0; i < 2; i++) {
+			for (let j = 0; j < 7; j++) {
+				square(y*2*j,y*4*i,y);
+			}
+			for (let j = 0; j < 7; j++) {
+				square(y*2*j+x,y+y*4*i,y);
+			}
+			for (let j = 0; j < 7; j++) {
+				square(y*2*j+2*x,2*y+y*4*i,y);
+			}
+			for (let j = 0; j < 7; j++) {
+				square(y*2*j+x,3*y+y*4*i,y);
+			}
+		}
+		for (let j = 0; j < 7; j++) {
+			square(y*2*j,y*8,y);
+		}
+	pop();
+}
+
+function drawLines(y) {  // Función para dibujar las líneas
+	push();
+		strokeWeight(3);
+		stroke(120);
+		for (let i = 1; i < 9; i++) {
+			line(0,y*i,width,y*i);
+		}
+	pop();
 }
