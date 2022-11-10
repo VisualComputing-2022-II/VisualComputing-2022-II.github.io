786f4c29970ff894483128a4759b2659a00c7205
jusruizsa <jusruizsa@users.noreply.github.com>
2022-11-10 00:25:36 +0000

    deploy: 95b56912dde6ee2535e60721be8d712482e14080

--- sketches/shaders/color/blend.js
+++ sketches/shaders/color/blend.js
@@ -1,38 +1,42 @@
+let uMaterial1
+let uMaterial2
+
 function preload() {
-    colorShader = readShader('blend.frag',  
-                    { matrices: Tree.pmvMatrix, varyings: Tree.color4 });
+  colorShader = readShader('blend.frag',
+                          { matrices: Tree.NONE, varyings: Tree.color4 });
 }
 
 function setup() {
-
-    createCanvas(300, 300, WEBGL);
-    colorPicker1 = createColorPicker('#fff000')
-    colorPicker2 = createColorPicker('#000fff')
-
-    colorPicker1.position(10, 0);
-    colorPicker2.position(80, 0);
-
-
+  createCanvas(400, 400, WEBGL);
+  colorPicker = createColorPicker('#ed225d')
+  colorPicker2 = createColorPicker('#ed225d')
 }
 
 function draw() {
-
-    resetShader()
-
-    background(255);
-    fill(colorPicker1.color())
-    square(-width/2 + 10, -height/2 + 50, 100)
-
-    fill(colorPicker2.color())
-    square(0, -height/2  + 50, 100)
-
-    shader(colorShader)
-    color1 = colorPicker1.color()
-    color2 = colorPicker2.color()
-    colorShader.setUniform('uMaterial1', [red(color1), green(color1), blue(color1), alpha(color1)])
-    colorShader.setUniform('uMaterial2', [red(color2), green(color2), blue(color2), alpha(color2)])
-    circle(0, 0, 100)
-
+  
+  background(255);
+  
+  //Square 1
+  fill(colorPicker.color())
+  square(0, 10, 50)
+  
+  //Square 2
+  fill(colorPicker2.color())
+  square(100, 10, 50)
+  
     
+  //Color multiplication with shader
+  push()
+    shader(colorShader)
+    uMaterial1 = colorPicker.color()._array
+    uMaterial2 = colorPicker2.color()._array
 
+    colorShader.setUniform('uMaterial1', uMaterial1)
+    colorShader.setUniform('uMaterial2', uMaterial2)
+    noStroke()
+    square(50, 50, 50)
+  pop()
+
+  resetShader()
+    
 }
